d27ea154a402b5874157f329a0388e863d6f09cc
Egor Pugin <egor.pugin@gmail.com>
2023-01-17 21:08:32 +0300

    Initial test infrastructure.

--- sw.h
+++ sw.h
@@ -1,28 +1,24 @@
 void build(solution &s) {
-    auto &fasta = s.add<executable>("fasta");
-    //fasta += "problem/fasta/solution/solution1.c";
-    fasta += "problem/fasta/solution/solution2.cpp";
-
-    auto &revcomp = s.add<executable>("revcomp");
-    //revcomp += "problem/reverse-complement/solution/solution1.c";
-    revcomp += "problem/reverse-complement/solution/solution2.cpp";
-
-    //auto &t = s.add<test>();
-    //t += fasta, 100000;
-
     static resource_pool pool = 1;
+    path fasta_output;
+    for (auto &&dir : {"fasta"s, "reverse-complement"s}) {
+        auto fulldir = s.source_dir / "problem" / dir / "solution";
+        for (path fn : fs::directory_iterator{fulldir}) {
+            auto &t = s.add<executable>(dir + "."s + fn.stem().string());
+            t += fn;
 
-    path p;
-    auto n = 10000000;
-    // auto n = 100000;
-    auto f = fasta.add_test("test");
-    {
-        f += n;
-        f.set_resource_pool(pool);
-    }
-    {
-        auto c = revcomp.add_test("test");
-        c < std::get<path>(f.out);
-        c.set_resource_pool(pool);
+            auto c = t.add_test();
+            c.set_resource_pool(pool);
+
+            // data for revcomp
+            if (dir == "fasta"s) {
+                c += 100000;
+                if (fn.filename() == "solution2.cpp") {
+                    fasta_output = std::get<path>(c.out);
+                }
+            } else if (dir == "reverse-complement"s) {
+                c < fasta_output;
+            }
+        }
     }
 }
