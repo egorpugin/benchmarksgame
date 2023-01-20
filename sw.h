2e06128fd342042469060a7921cf257b781546a7
Egor Pugin <egor.pugin@gmail.com>
2023-01-20 05:00:44 +0300

    Update.

--- sw.h
+++ sw.h
@@ -4,11 +4,26 @@
 
 void build(solution &s) {
     static resource_pool pool = 1;
+
+    int fasta_size = 100000;
+    int revcomp_size = fasta_size;
     path fasta_output;
-    for (auto &&dir : {"fasta"s, "reverse-complement"s}) {
+    {
+        auto &t = s.add<executable>("data_generator");
+        t += "problem/revcomp/data_generator.cpp";
+        auto c = t.add_command();
+        c += revcomp_size;
+        c > t.binary_dir / "data.txt";
+        fasta_output = c.out;
+    }
+
+    for (auto &&dir : {"fasta"s, "revcomp"s}) {
         auto fulldir = s.source_dir / "problem" / dir / "solution";
         for (path fn : fs::recursive_directory_iterator{fulldir}) {
             if (!fs::is_regular_file(fn) || fn.filename().string().starts_with(".")) {
+                continue;
+            }
+            if (!(fn.extension() == ".c" || fn.extension() == ".cpp")) {
                 continue;
             }
             auto namep = fn.lexically_relative(fulldir);
@@ -22,13 +37,10 @@ void build(solution &s) {
             auto c = t.add_test(dir + "."s + name);
             c.set_resource_pool(pool);
 
-            if (dir == "fasta"s) {
-                c += 100000;
-                if (fn.filename() == "solution2.cpp") {
-                    // data for revcomp etc.
-                    fasta_output = c.out;
-                }
-            } else if (dir == "reverse-complement"s) {
+            if (0) {
+            } else if (dir == "fasta"s) {
+                c += fasta_size;
+            } else if (dir == "revcomp"s) {
                 c < fasta_output;
             }
         }
